f04415b1a39fd9873b28ce764984aa458a35d689
Aniruddha Wagle K <aniruddhawagle@gmail.com>
2025-03-12 17:01:26 +0530

    Add Joystick

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -20,6 +20,12 @@ def generate_launch_description():
         )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
     )
 
+    joystick = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory(package_name),'launch','joystick.launch.py'
+        )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
     gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
     
     # Gazebo launch file, provided by the gazebo_ros package
@@ -53,6 +59,7 @@ def generate_launch_description():
 
     return LaunchDescription([
         rsp,
+        joystick,
         gazebo,
         spawn_entity,
         diff_drive_spawner,
